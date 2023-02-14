0fcf3fdaf59ffcf43c2f55c3bcbb7e4838cb01a6
Abu Masum Didar <masumdidar.cse@gmail.com>
2023-02-14 19:08:25 +0600

    Complete a crud application with discount method

--- Crud Apps with Ruby/CRUD.rb
+++ Crud Apps with Ruby/CRUD.rb
@@ -1,11 +1,15 @@
 class Product
-  attr_accessor :title, :price, :discount, :discount_price
+  attr_accessor :title, :price, :discount
 
-  def initialize(title, price, discount, discount_price)
+  def initialize(title, price, discount)
     @title = title
     @price = price
     @discount = discount
-    @discount_price = discount_price
+  end
+
+  def after_discount_price
+    discount_percent = @discount.delete('%').to_f / 100.0
+    discount_price = @price - (@price * discount_percent)
   end
 end
 
@@ -14,25 +18,31 @@ class Offer
     @products = []
   end
 
-  def add_product(title, price, discount, discount_price)
-    product = Product.new(title, price, discount, discount_price)
+  def add_product(title, price, discount)
+    product = Product.new(title, price, discount)
     @products << product
     product
   end
 
   def all_products
-    @products
+    @products.map do |product|
+      {
+        title: product.title,
+        price: product.price,
+        discount: "#{(product.discount * 100).to_i}%",
+        discount_price: product.after_discount_price
+      }
+    end
   end
 
   def find_product(title)
     @products.find {|product| product.title == title }
   end
 
-  def update_product(title, price, discount, discount_price)
+  def update_product(title, price, discount)
     product = find_product(title)
     product.price = price
     product.discount = discount
-    product.discount_price = discount_price
   end
 
   def remove_product(title)
@@ -42,12 +52,20 @@ class Offer
 end
 
 offer = Offer.new
-product1 = offer.add_product("T-Shirt", 250, "20%", 200)
-product2 = offer.add_product("Shirt", 500, "20%", 400)
-product3 = offer.add_product("Panjabhi", 1000, "20%", 800)
+product1 = offer.add_product("T-Shirt", 250, "10%")
+product2 = offer.add_product("Shirt", 500, "20%")
+product3 = offer.add_product("Panjabhi", 1000, "20%")
 
 p "............"
 puts offer.all_products.inspect
+
+puts "All products with discounted prices:"
+offer.all_products.each do |product|
+  puts "Title: #{product[:title]},
+        Price: #{product[:price]},
+        Discount: #{product[:discount]},
+        Discounted Price: #{product[:discount_price]}"
+end
 
 p "............"
 
@@ -56,7 +74,7 @@ puts single_product.inspect
 
 p "............"
 
-offer.update_product("Panjabhi", 2500, "20%", 2000)
+offer.update_product("Panjabhi", 2500, "20%")
 puts product3.inspect
 
 p "............"
